a7e9465bc1db1f0946affeff2af52c1c2937f844
Jesse Roy-Cote <jesseroyc@gmail.com>
2018-02-18 20:11:36 -0700

    Searches by name and outputs values properly

--- lookup_people.js
+++ lookup_people.js
@@ -1,5 +1,6 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
+const args = process.argv;
 
 const client = new pg.Client({
   user     : settings.user,
@@ -10,23 +11,20 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
-client.connect( (err) => {
+console.log("Searching...");
+
+client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-
-  // execute a query on our database
-  client.query('SELECT $1::text as name', [args[2]], function (err, result) {
+  client.query(`SELECT * FROM famous_people WHERE first_name = '${args[2]}';`, (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
 
-
-    // just print the result to the console
     console.log(`Found 1 person(s) by the name "${args[2]}":\n 
-        ${result.rows[0].first_name} ${result.rows[0].last_name}, born '${result.rows[0].birthdate}'`;
-
-    // disconnect the client
+        ${result.rows[0].first_name} ${result.rows[0].last_name}, born "${result.rows[0].birthdate}"`);
+    
     client.end();
   });
 });
